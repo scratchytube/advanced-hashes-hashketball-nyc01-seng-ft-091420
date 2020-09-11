25680c60fef94aecc12eb254ca0ccfaf6f9f8dc3
Brian Sahota <bpsahota@gmail.com>
2020-09-11 16:37:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -203,7 +203,7 @@ end
 
 def big_shoe_rebounds 
   big_shoe = 0 
-  rebound = 0 
+  rebounds = 0 
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
       if player[:shoe] > big_shoe 
