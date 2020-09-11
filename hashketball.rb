59a7a8f9770501168aac125872df46679599a1df
Brian Sahota <bpsahota@gmail.com>
2020-09-11 16:21:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -147,7 +147,15 @@ def num_points_scored(player_search)
   end
 end
 
-def shoe_size()
+def shoe_size(name)
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[player_name] == name 
+        return player[:shoe]
+      end 
+    end 
+  end 
+end
 
 
 
